d9d9a8d1b5cf5675072991f01c490cfb1ddb0ee4
Jason Thorpe <jdthorpe@gmail.com>
2019-05-16 16:56:23 -0700

    adding batch client

--- src/SparseSC/cli/stt.py
+++ src/SparseSC/cli/stt.py
@@ -14,6 +14,26 @@ except ImportError:
 from SparseSC.cross_validation import score_train_test
 
 
+def get_config(infile):
+    """
+    read in the contents of the inputs yaml file
+    """
+
+    with open(infile, "r") as fp:
+        config = load(fp, Loader=Loader)
+    try:
+        v_pen = tuple(config["v_pen"])
+    except TypeError:
+        v_pen = (config["v_pen"],)
+
+    try:
+        w_pen = tuple(config["w_pen"])
+    except TypeError:
+        w_pen = (config["w_pen"],)
+
+    return v_pen, w_pen, config
+
+
 def main():
     # GET THE COMMAND LINE ARGS
     ARGS = sys.argv[1:]
@@ -25,23 +45,13 @@ def main():
     infile, outfile, batchNumber = ARGS
     batchNumber = int(batchNumber)
 
-    with open(infile, "r") as fp:
-        config = load(fp, Loader=Loader)
+    v_pen, w_pen, config = get_config(infile)
+    n_folds = len(config["folds"]) * len(v_pen) * len(w_pen)
 
-    try: 
-        v_pen = tuple(config["v_pen"])
-    except TypeError:
-        v_pen = (config["v_pen"],)
-
-    try: 
-        w_pen = tuple(config["w_pen"])
-    except TypeError:
-        w_pen = (config["w_pen"],)
-
-    assert 0 <= batchNumber < len(config["folds"]) * len(v_pen) * len(w_pen), "Batch number out of range"
+    assert 0 <= batchNumber < n_folds, "Batch number out of range"
     i_fold = batchNumber % len(config["folds"])
-    i_v = (batchNumber // len(config["folds"]) ) % len(v_pen)
-    i_w = (batchNumber // len(config["folds"]) ) // len(v_pen)
+    i_v = (batchNumber // len(config["folds"])) % len(v_pen)
+    i_w = (batchNumber // len(config["folds"])) // len(v_pen)
 
     params = config.copy()
     del params["folds"]
@@ -49,11 +59,24 @@ def main():
     del params["w_pen"]
 
     train, test = config["folds"][i_fold]
-    out = score_train_test(train=train,test=test,v_pen=v_pen[i_v],w_pen=w_pen[i_w],**params)
-    
+    out = score_train_test(
+        train=train, test=test, v_pen=v_pen[i_v], w_pen=w_pen[i_w], **params
+    )
+
     with open(outfile, "w") as fp:
-        fp.write(dump({"batch":batchNumber, "results": out}, Dumper=Dumper))
-    
+        fp.write(
+            dump(
+                {
+                    "batch": batchNumber,
+                    "i_fold": i_fold,
+                    "i_v": i_v,
+                    "i_w": i_w,
+                    "results": out,
+                },
+                Dumper=Dumper,
+            )
+        )
+
+
 if __name__ == "__main__":
     main()
-
