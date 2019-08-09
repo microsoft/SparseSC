4c1adce02d7f8006e64e0ab1d1437d933cc4d57d
Brian Quistorff <Brian.Quistorff@microsoft.com>
2019-08-09 14:01:03 -0700

    Add a basic transformer

--- src/SparseSC/utils/misc.py
+++ src/SparseSC/utils/misc.py
@@ -33,3 +33,29 @@ def capture_all():
         sys.stdout,sys.stderr = oldout, olderr
         out[0] = out[0].getvalue()
         out[1] = out[1].getvalue()
+
+class PreDemeanScaler:
+    '''
+    Units are defined by rows and cols are "pre" and "post" separated.
+    Demeans each row by the "pre" mean.
+    '''
+    #maybe fit should just take Y and T0 (in init())? 
+    # Try in sklearn.pipeline with fit() for that and predict (on default Y_post)
+    # might want wrappers around fit to make that work fine with pipeline (given its standard arguments).
+    #maybe call the vars X rather than Y?
+    def __init__(self):
+        self.means = None
+        #self.T0 = T0
+
+    def fit(self, Y):
+        '''
+        Ex. fit(Y.iloc[:,0:T0])
+        '''
+        import numpy as np
+        self.means = np.mean(Y, axis=1)
+    
+    def transform(self, Y):
+        return (Y.T - self.means).T
+
+    def inverse_transform(self, Y):
+        return (Y.T + self.means).T
